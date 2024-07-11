ac0b923a1744e79cb29d5404d38c746836ba06db
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2024-07-10 19:16:28 -0700

    Patch request done

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 		return c.Status(200).JSON(fiber.Map{"msg": "app is created"})
 	})
 
+	// post a todo
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
 		todo := &Todo{} // {id: 0, completed: false, body: ""}
 		if err := c.BodyParser(todo); err != nil {
@@ -36,10 +37,21 @@ func main() {
 		todo.ID = len(todos) + 1
 		todos = append(todos, *todo)
 
-		
-
 		return c.Status(201).JSON(todo)
 	})
 
+	// update a todo
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos[i].Completed = true
+				return c.Status(200).JSON(todos[i])
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	})
 	log.Fatal(app.Listen(":4000"))
 }
