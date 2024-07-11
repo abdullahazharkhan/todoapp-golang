b7fe5f4c3ea58211430617d43328f291886fb507
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2024-07-10 19:19:53 -0700

    Get Request done

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ func main() {
 
 	todos := []Todo{}
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{"msg": "app is created"})
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(todos)
 	})
 
 	// post a todo
