348849b176acd4eacd797dd84312ac57c4ff8380
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2024-07-13 01:48:00 -0700

    added files for the UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
-import {Button} from "@chakra-ui/react"
+import {Button, Container, Stack} from "@chakra-ui/react"
+import Navbar from "./components/Navbar"
+import TodoForm from "./components/TodoForm"
+import TodoList from "./components/TodoList"
 
 function App() {
 
   return (
-    <>
-      <Button>hello</Button>
-    </>
+    <Stack h={"100vh"}>
+      <Navbar />
+      <Container>
+        <TodoForm />
+        <TodoList />
+      </Container>
+    </Stack>
   )
 }
 
